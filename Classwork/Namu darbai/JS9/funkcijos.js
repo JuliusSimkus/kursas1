d941230973cb4d2a899295ec95b45864fe1400ce
JuliusSimkus <julius.simkus0@gmail.com>
2018-12-19 23:05:08 +0200

    pagaliau js9

--- Classwork/Namu darbai/JS9/funkcijos.js
+++ Classwork/Namu darbai/JS9/funkcijos.js
@@ -1,42 +1,25 @@
-function lentele(viesbuciai) {
-         document.write ("<h2>" + viesbuciai[i].name + "</h2>");
-         document.write ("<h3>" + viesbuciai[i].Address + "</h3>");
-         document.write ("<table border=1>");
-         document.write ("<tr>");
-         document.write("<td>" + "Kambario numeris" + "</td><td>" + "Žmonių skaičius" + "</td><td>" + "Kaina" + "</td>");
-         document.write ("</tr>");                            
-         document.write ("<tr>");
-         document.write("<td>" +viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" + viesbuciai[i].Rooms[i].Price + "</td>");
-         document.write ("</tr>");                         
-         document.write ("<tr>");
-         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" + viesbuciai[i].Rooms[i].Price + "</td>");
-         document.write ("</tr>");          
-         document.write ("<tr>");
-         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" + viesbuciai[i].Rooms[i].Price + "</td>");
-         document.write ("</tr>");
-          document.write ("<tr>");
-         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" +viesbuciai[i].Rooms[i].Price + "</td>");
-         document.write ("</tr>");
-         document.write ("</table>") 
-         
-//         document.write ("<h2>" + viesbuciai[i].name + "</h2>");
-//         document.write ("<h3>" + viesbuciai[i].Address + "</h3>");
-//         document.write ("<table border=1>");
-//         document.write ("<tr>");
-//         document.write("<td>" + "Kambario numeris" + "</td><td>" + "Žmonių skaičius" + "</td><td>" + "Kaina" + "</td>");
-//         document.write ("</tr>");                            
-//         document.write ("<tr>");
-//         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" + viesbuciai[i].Rooms[i].Price + "</td>");
-//         document.write ("</tr>");                         
-//         document.write ("<tr>");
-//         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" + viesbuciai[i].Rooms[i].Price + "</td>");
-//         document.write ("</tr>");          
-//         document.write ("<tr>");
-//         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" + viesbuciai[i].Rooms[i].Price + "</td>");
-//         document.write ("</tr>");   
-//          document.write ("<tr>");
-//         document.write("<td>" + viesbuciai[i].Rooms[i].Number + "</td><td>" + viesbuciai[i].Rooms[i].GuestCount + "</td><td>" +viesbuciai[i].Rooms[i].Price + "</td>");
-//         document.write ("</tr>");           
-//         document.write ("</table>") 
-         
-      }
+
+function lentele(viesbuciai) {       
+   
+           for (i = 0;i < viesbuciai.length; i ++){
+               document.write ("<table border=1>");
+               document.write ("<h2>" + viesbuciai[i].name + "</h2>");
+               document.write ("<h3>" + viesbuciai[i].Address + "</h3>");
+               document.write ("<tr>");
+               document.write("<td>" + "Kambario numeris" + "</td><td>" + "Žmonių skaičius" + "</td><td>" + "Kaina" + "</td>");
+               document.write ("</tr>");
+              
+              for(j = 0; j < viesbuciai[i].Rooms.length; j++){
+                 
+                  document.write ("<tr>");
+                  document.write("<td>" +viesbuciai[i].Rooms[j].Number + "</td><td>" + viesbuciai[i].Rooms[j].GuestCount + "</td><td>" + viesbuciai[i].Rooms[j].Price + "</td>");
+                  document.write ("</tr>"); 
+                  
+               }
+              
+            }   
+            document.write ("</table>")
+
+         }
+
+   
