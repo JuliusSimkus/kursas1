e6ff34ee57eec31fb0de4943f061925498f1a4fa
JuliusSimkus <julius.simkus0@gmail.com>
2019-01-02 22:27:34 +0200

    pridetas php03

---
+++ Classwork/Namu darbai/PHP03/index03.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title></title>
+</head>
+
+<body>
+   
+   <?php 
+      $x = 10;
+      $y = 7;
+   ?>
+   <?php $veiksmas = $x + $y; ?>   
+   <?php echo "$x + $y = $veiksmas"; ?> <br>
+   
+   <?php $veiksmas = $x - $y; ?>
+   <?php  echo "$x - $y = $veiksmas"; ?> <br>
+   
+   <?php $veiksmas = $x * $y; ?>
+   <?php  echo "$x * $y = $veiksmas"; ?> <br>
+   
+   <?php $veiksmas = $x / $y; ?>
+   <?php  echo "$x / $y = $veiksmas"; ?> <br>
+   
+   <?php $veiksmas = $x % $y; ?>
+   <?php  echo "$x % $y = $veiksmas"; ?>
+   
+</body>
+</html>
+
+
