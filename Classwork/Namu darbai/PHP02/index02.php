997abd47c6de0d28ea432fb25454a634989a0348
JuliusSimkus <julius.simkus0@gmail.com>
2019-01-02 20:48:33 +0200

    pridetas php02

---
+++ Classwork/Namu darbai/PHP02/index02.php
@@ -0,0 +1,18 @@
+<?php $country = 'Lietuva'; ?>
+<?php $city = 'Vilnius'; ?>
+<?php $population = 574221; ?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title></title>
+</head>
+
+<body>
+
+   <p><?= $city . ' - Lietuvos sostinė.'?></p>
+   <p><?= "$city ir $country sparčiai auga"?></p>
+   
+</body>
+</html>
