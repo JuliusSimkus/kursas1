d8b319390729166b80d6615e4a9363963d0d9d36
JuliusSimkus <julius.simkus0@gmail.com>
2019-01-02 20:35:44 +0200

    pridetas php01

---
+++ Classwork/Namu darbai/PHP01/index1.php
@@ -0,0 +1,19 @@
+<?php $country = 'Lietuva'; ?>
+<?php $city = 'Vilnius'; ?>
+<?php $population = 574221; ?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title></title>
+</head>
+
+<body>
+   <ul>
+      <li><?php echo $country; ?></li>
+      <li><?php echo $city; ?></li>
+      <li><?php echo $population; ?></li>
+   </ul>
+</body>
+</html>
