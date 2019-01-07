de8f5298ec15f60933fc28cc40c53b3429c30e83
JuliusSimkus <julius.simkus0@gmail.com>
2019-01-07 18:35:05 +0200

    pridetas 08

--- Classwork/Namu darbai/PHP08/index08.php
+++ Classwork/Namu darbai/PHP08/index08.php
@@ -1,39 +1,38 @@
 <?php
    $temp = [4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21];
-?>
 
-<br>
 
-<?php
+echo "<br>";
+
+
    $averageTemp = array_sum($temp) / count($temp); 
    echo "Vidutinė temperatūra yra: " . $averageTemp;
-?> 
-   <br>
-   
-<?php
-   echo "Suapvalinta vidutinė temperatūra yra: " . round($averageTemp);
-?>  
-   
-   <br>
-   
-<?php
-   $temp = [4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21];
-   rsort($temp);
-?>   
 
-Penkios šilčiausios temp:   
-<?php                                
+echo "<br>";
+   
+
+   echo "Suapvalinta vidutinė temperatūra yra: " . round($averageTemp);
+  
+   
+echo "<br>";
+   
+
+   rsort($temp);
+  
+
+echo "Penkios šilčiausios temp:";   
+                               
    $siluma = array_slice($temp,0,5);
    foreach ($siluma as $siltasSkaicius){
    echo $siltasSkaicius . " " ;
    }
- ?>     
+     
 
-<br> 
+echo "<br>";
    
-Penkios šalčiausios temp:
-   
-<?php   
+echo "Penkios šalčiausios temp:";
+
+
    $saltis = array_slice($temp,-5,5);
    foreach ($saltis as $saltasSkaicius){
    echo $saltasSkaicius . " " ;   
